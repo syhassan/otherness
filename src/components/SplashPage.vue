b81ba3f1f8965e2dc6e018c18124553e371f3c0e
syhassan <rosh.hass@gmail.com>
2024-01-02 14:53:01 +0100

    colours

--- src/components/SplashPage.vue
+++ src/components/SplashPage.vue
@@ -22,30 +22,32 @@
       <div class="py-10"/>
       <div class="py-10"/>
       <div class="py-10"/>
+      <div class="py-10"/>
+      <div class="py-10"/>
       
       <v-container fluid style="height:1200px; width:1000px; position:absolute; top: 20%; left: 0%;">
         <div class="one">
-          <div class=" font-italic font-weight-bold">Community</div>
+          <div class=" font-italic">Community</div>
         </div>
 
         <div class="two">
-          <div class=" font-italic font-weight-bold">Acceptance</div>
+          <div class=" font-italic">Acceptance</div>
         </div>
 
         <div class="three">
-          <div class=" font-italic font-weight-bold">Connection</div>
+          <div class=" font-italic">Connection</div>
         </div>
         
         <div class="four">
-          <div class=" font-italic font-weight-bold">Love</div>
+          <div class=" font-italic">Love</div>
         </div>
         
         <div class="five">
-          <div class=" font-italic font-weight-bold">Identity</div>
+          <div class=" font-italic">Identity</div>
         </div>
 
         <div class="six">
-          <div class=" font-italic font-weight-bold">Self-image</div>
+          <div class=" font-italic">Self-image</div>
         </div>
       </v-container>
  
@@ -80,12 +82,13 @@ p {
   margin-top: -40px;
 }
 .one {
+  opacity: 75%;
   height: 300px;
   width: 300px;
   line-height: 300px;
   border-radius: 50%;
-  background: rgba(255,0,0,0.7);
-  top: 50%;
+  background: #53917E;
+  top: 40%;
   left: 54%;
   margin: 0 auto;
   position: absolute;
@@ -93,60 +96,65 @@ p {
 }
 
 .two {
+  opacity: 75%;
   height: 400px;
   width: 400px;
   line-height: 400px;
   border-radius: 50%;
-  background: rgba(63,102,52,0.7);
-  top: 40%;
+  background: #FA7921;
+  top: 30%;
   left: 19%;
   margin: 0 auto;
   position: absolute;
   text-align: center;
 }
 .three {
+  opacity: 75%;
   height: 200px;
   width: 200px;
   line-height: 200px;
   border-radius: 50%;
-  background: rgba(89,86,229,0.7);
-  top: 45%;
+  background: #FCD757;
+  top: 35%;
   left: 9%;
   margin: 0 auto;
   position: absolute;
   text-align: center;
 }
 .four {
+  opacity: 75%;
   height: 250px;
   width: 250px;
   line-height: 250px;
   border-radius: 50%;
-  background: rgba(233,196,106,0.7);
-  top: 60%;
+  background: #716FCE;
+  top: 50%;
   left: 9%;
   margin: 0 auto;
   position: absolute;
   text-align: center;
 }
 .five {
+  opacity: 75%;
   height: 250px;
   width: 250px;
   line-height: 250px;
   border-radius: 50%;
-  background: rgba(236,159,5,0.7);
-  top: 60%;
+  background: #D90D0D;
+  top: 50%;
   left: 29%;
   margin: 0 auto;
   position: absolute;
   text-align: center;
 }
 .six {
+  opacity: 75%;
   height: 150px;
   width: 150px;
   line-height: 150px;
   border-radius: 50%;
-  background: rgba(245,187,0,0.7);
-  top: 40%;
+  background: #BF67AB;
+  top: 30%;
   left: 49%;
   margin: 0 auto;
   position: absolute;
